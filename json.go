acd7449b9c929b0b4989f45daa802bfee99e9f6e
per1Peteia <justusmanthey@googlemail.com>
2025-01-28 12:18:49 +0100

    Revert "Merge pull request #6 from per1Peteia/addtests"
    This reverts commit 47300879d680ac167ec2b9c473ab0af80fc44f38, reversing
    changes made to 2cdadfa67f7f4af4407ad8faae90f83fc5c5e793.

--- json.go
+++ json.go
@@ -27,9 +27,5 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	if _, err := w.Write(dat); err != nil {
-		log.Printf("Error writing response: %s", err)
-		w.WriteHeader(500)
-		return
-	}
+	w.Write(dat)
 }
